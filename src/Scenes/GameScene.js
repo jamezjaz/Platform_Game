1fcaf97f0e409a744bf3f310f21ac0d374138f3c
jamezjaz <jamezjaz@gmail.com>
2020-12-16 14:54:02 -0600

    docs: adds collider between player sprite and the platforms

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,7 +1,7 @@
 import 'phaser';
 import OptionsScene from './OptionsScene';
 
-var platforms
+let platforms
 let player
 let cursors
 let stars
@@ -9,7 +9,7 @@ let score = 0;
 let scoreText;
 let gameOverText
 let bombs
-var gameOver = false;
+let gameOver = false;
  
 export default class GameScene extends Phaser.Scene {
   constructor () {
@@ -38,5 +38,32 @@ export default class GameScene extends Phaser.Scene {
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.physics.add.collider(player, platforms);
   }
 };
