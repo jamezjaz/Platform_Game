5c257d4445cfb10158f4a3dae061c32dae95c270
jamezjaz <jamezjaz@gmail.com>
2020-12-16 07:50:32 -0600

    docs: implements feature for stopping bgMusic

--- src/Scenes/OptionsScene.js
+++ src/Scenes/OptionsScene.js
@@ -40,8 +40,14 @@ export default class OptionsScene extends Phaser.Scene {
   updateAudio() {
     if (this.model.musicOn === false) {
       this.musicButton.setTexture('box');
+      this.sys.game.globals.bgMusic.stop();
+      this.model.bgMusicPlaying = false;
     } else {
       this.musicButton.setTexture('checkedBox');
+      if (this.model.bgMusicPlaying === false) {
+        this.sys.game.globals.bgMusic.play();
+        this.model.bgMusicPlaying = true;
+      }
     }
 
     if (this.model.soundOn === false) {
--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -23,9 +23,10 @@ export default class TitleScene extends Phaser.Scene {
     //backgroud music from the glodel model state
     this.model = this.sys.game.globals.model;
     if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
-      this.model.bgMusicPlaying = true;
       this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
       this.bgMusic.play();
+      this.model.bgMusicPlaying = true;
+      this.sys.game.globals.bgMusic = this.bgMusic;
     }
   }
 };
