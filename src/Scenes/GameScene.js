a52059c8a02a6e77e4f067b53851e6fa2de6d79a
jamezjaz <jamezjaz@gmail.com>
2020-12-16 15:14:58 -0600

    docs: add Phaser's built-in keyboard manager and player animations

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -65,5 +65,33 @@ export default class GameScene extends Phaser.Scene {
     });
 
     this.physics.add.collider(player, platforms);
+
+    cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
   }
 };
