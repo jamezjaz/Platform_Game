24f3e4d1a490de0dd87a6612f091005308a08a94
jamezjaz <jamezjaz@gmail.com>
2020-12-16 02:28:15 -0600

    docs: populates UI buttons on the Title scene

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -1,13 +1,23 @@
 import 'phaser';
+import config from '../Config/config';
+import Button from '../Objects/Button';
  
 export default class TitleScene extends Phaser.Scene {
   constructor () {
     super('Title');
   }
  
-  preload () {
-  }
+  // preload () {
+  // }
  
   create () {
+    // Game
+    this.gameButton = new Button(this, config.width/2, config.height/2 - 150, 'blueButton1', 'blueButton2', 'Play', 'Input');
+
+    // Options
+    this.optionsButton = new Button(this, config.width/2, config.height/2 - 50, 'blueButton1', 'blueButton2', 'Options', 'Options');
+
+    // Credits
+    this.creditsButton = new Button(this, config.width/2, config.height/2 + 50, 'blueButton1', 'blueButton2', 'Credits', 'Credits');
   }
 };
