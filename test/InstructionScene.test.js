5951b327ab520184441ea39a9ae8864e94f72efa
jamezjaz <jamezjaz@gmail.com>
2020-12-21 13:42:22 -0600

    docs: adds InstructionScene test

---
+++ test/InstructionScene.test.js
@@ -0,0 +1,7 @@
+import Phaser from 'phaser';
+import InstructionScene from '../src/Scenes/InstructionScene';
+require('jest-expect-subclass');
+
+test('Bootscene should be a subclass of Phaser.Scene', () => {
+  expect(InstructionScene).toBeSubclassOf(Phaser.Scene);
+});
