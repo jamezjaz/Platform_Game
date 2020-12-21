abd0892ec3fd6b2e2d267c2ee6bdb6a92fcac37e
jamezjaz <jamezjaz@gmail.com>
2020-12-21 09:57:27 -0600

    docs: adds PreloaderScene test

---
+++ test/PreloaderScene.test.js
@@ -0,0 +1,7 @@
+import Phaser from 'phaser';
+import PreloaderScene from '../src/Scenes/PreloaderScene';
+require('jest-expect-subclass');
+
+test('PreloaderScene should be a subclass of Phaser.Scene', () => {
+  expect(PreloaderScene).toBeSubclassOf(Phaser.Scene);
+});
